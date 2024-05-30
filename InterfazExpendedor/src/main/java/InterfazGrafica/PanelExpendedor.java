5d387f2bc0cd13655ac3edc89caa03a4ddb5a8cf
d-a-t-s <dtamaro@udec.cl>
2024-05-29 22:19:45 -0400

    Agrego el modelo grafico del Expendedor y los modelos de los productos + inicio de la clase PanelDepositoCoca

--- InterfazExpendedor/src/main/java/InterfazGrafica/PanelExpendedor.java
+++ InterfazExpendedor/src/main/java/InterfazGrafica/PanelExpendedor.java
@@ -1,20 +1,26 @@
 package InterfazGrafica;
 
+import Logica.*;
 import javax.swing.*;
 import java.awt.*;
 
 public class PanelExpendedor extends JPanel{
     private Image imagenExpendedor;
+    private PanelDepositoCoca panelDepositoCoca;
+    private Expendedor expendedor;
     //Constructor
     public PanelExpendedor(){
         super();
-        imagenExpendedor = new ImageIcon("Resources/test.jpg").getImage();
+        imagenExpendedor = new ImageIcon(getClass().getClassLoader().getResource("Expendedor.png")).getImage();
         Dimension size = new Dimension(imagenExpendedor.getWidth(null), imagenExpendedor.getHeight(null));
         this.setPreferredSize(size);
         this.setMinimumSize(size);
         this.setMaximumSize(size);
         this.setSize(size);
         this.setLayout(null);
+
+        panelDepositoCoca = new PanelDepositoCoca();
+        this.add(panelDepositoCoca);
     }
     @Override
     public void paintComponent(Graphics g){
--- InterfazExpendedor/src/main/java/InterfazGrafica/Ventana.java
+++ InterfazExpendedor/src/main/java/InterfazGrafica/Ventana.java
@@ -5,11 +5,15 @@ import java.awt.*;
 
 public class Ventana extends JFrame{
     private PanelExpendedor panelExpendedor;
+    private PanelDepositoCoca panelDepositoCoca;
     //Constructor
     public Ventana(){
         super();
         this.setTitle("Expendedor");
         this.setLayout(new BorderLayout());
+
+        //Tests
+
 
         panelExpendedor = new PanelExpendedor();
         this.add(panelExpendedor, BorderLayout.CENTER);
